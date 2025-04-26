5e8c9bd596d1840e54298aeed714f6f834762ec6
Marc-Alx <marc-alx@outlook.com>
2025-04-26 20:19:40 +0200

    [assets] add light font

--- source/gameAssets.lua
+++ source/gameAssets.lua
@@ -1,6 +1,7 @@
 local gfx  <const> = playdate.graphics
 
 GameAssets = {
+    SMALL_FONT            = gfx.font.new("assets/game/fonts/Nontendo/Nontendo-Light"),
     NORMAL_FONT            = gfx.font.new("assets/game/fonts/Nontendo/Nontendo-Bold"),
     LARGE_FONT             = gfx.font.new("assets/game/fonts/Nontendo/Nontendo-Bold-2x")
 }
