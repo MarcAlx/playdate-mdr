de77ae6c58dd8d94e205757119aa58d5b80980a2
Marc-Alx <marc-alx@outlook.com>
2025-04-26 20:19:58 +0200

    [wip] add folder

--- source/main.lua
+++ source/main.lua
@@ -32,15 +32,35 @@ WIDTH = 250
 HEIGHT = 100
 PADDING = 4
 HEADER_HEIGHT = 27
-GRID_HEIGHT = 190
-FOLDER_HEIGHT = 215
+GRID_HEIGHT = 180
+FOLDER_HEIGHT = 219
 
-GRID_AREA = playdate.geometry.rect.new(15, HEADER_HEIGHT+2, 372, 160)
+GRID_AREA = playdate.geometry.rect.new(15, HEADER_HEIGHT+2, 372, 150)
 PROGRESS_BAR = playdate.geometry.rect.new(12, 9, 300, 14)
+
+FOLDER_OFFSET = 30
+FOLDER_SPACING = 15
+FOLDER_PROGRESS_WIDTH  = 50
+FOLDER_PROGRESS_HEIGHT = 14
+FOLDER_1   = playdate.geometry.rect.new(FOLDER_OFFSET + (1 * FOLDER_SPACING) + (0*FOLDER_PROGRESS_WIDTH) , GRID_HEIGHT + 4                              , FOLDER_PROGRESS_WIDTH , FOLDER_PROGRESS_HEIGHT)
+PROGRESS_1 = playdate.geometry.rect.new(FOLDER_OFFSET + (1 * FOLDER_SPACING) + (0*FOLDER_PROGRESS_WIDTH) , GRID_HEIGHT + (1*FOLDER_PROGRESS_HEIGHT) + 8 , FOLDER_PROGRESS_WIDTH , FOLDER_PROGRESS_HEIGHT)
+FOLDER_2   = playdate.geometry.rect.new(FOLDER_OFFSET + (2 * FOLDER_SPACING) + (1*FOLDER_PROGRESS_WIDTH) , GRID_HEIGHT + 4                              , FOLDER_PROGRESS_WIDTH , FOLDER_PROGRESS_HEIGHT)
+PROGRESS_2 = playdate.geometry.rect.new(FOLDER_OFFSET + (2 * FOLDER_SPACING) + (1*FOLDER_PROGRESS_WIDTH) , GRID_HEIGHT + (1*FOLDER_PROGRESS_HEIGHT) + 8 , FOLDER_PROGRESS_WIDTH , FOLDER_PROGRESS_HEIGHT)
+FOLDER_3   = playdate.geometry.rect.new(FOLDER_OFFSET + (3 * FOLDER_SPACING) + (2*FOLDER_PROGRESS_WIDTH) , GRID_HEIGHT + 4                              , FOLDER_PROGRESS_WIDTH , FOLDER_PROGRESS_HEIGHT)
+PROGRESS_3 = playdate.geometry.rect.new(FOLDER_OFFSET + (3 * FOLDER_SPACING) + (2*FOLDER_PROGRESS_WIDTH) , GRID_HEIGHT + (1*FOLDER_PROGRESS_HEIGHT) + 8 , FOLDER_PROGRESS_WIDTH , FOLDER_PROGRESS_HEIGHT)
+FOLDER_4   = playdate.geometry.rect.new(FOLDER_OFFSET + (4 * FOLDER_SPACING) + (3*FOLDER_PROGRESS_WIDTH) , GRID_HEIGHT + 4                              , FOLDER_PROGRESS_WIDTH , FOLDER_PROGRESS_HEIGHT)
+PROGRESS_4 = playdate.geometry.rect.new(FOLDER_OFFSET + (4 * FOLDER_SPACING) + (3*FOLDER_PROGRESS_WIDTH) , GRID_HEIGHT + (1*FOLDER_PROGRESS_HEIGHT) + 8 , FOLDER_PROGRESS_WIDTH , FOLDER_PROGRESS_HEIGHT)
+FOLDER_5   = playdate.geometry.rect.new(FOLDER_OFFSET + (5 * FOLDER_SPACING) + (4*FOLDER_PROGRESS_WIDTH) , GRID_HEIGHT + 4                              , FOLDER_PROGRESS_WIDTH , FOLDER_PROGRESS_HEIGHT)
+PROGRESS_5 = playdate.geometry.rect.new(FOLDER_OFFSET + (5 * FOLDER_SPACING) + (4*FOLDER_PROGRESS_WIDTH) , GRID_HEIGHT + (1*FOLDER_PROGRESS_HEIGHT) + 8 , FOLDER_PROGRESS_WIDTH , FOLDER_PROGRESS_HEIGHT)
 
 offsetX = -200
 offsetY = -200
 progress = 0
+frolder1Progress = 0
+frolder2Progress = 0
+frolder3Progress = 0
+frolder4Progress = 0
+frolder5Progress = 0
 
 numbers= {}
 function initNumbers()
@@ -80,6 +100,7 @@ function render()
     gfx.clear(gfx.kColorBlack)
 
     --progress
+    gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
     gfx.drawText(progress .. "% Complete", 30, 11)
     gfx.drawRect(PROGRESS_BAR)
 
@@ -93,13 +114,35 @@ function render()
     --start folder
     gfx.drawLine(PADDING,GRID_HEIGHT,playdate.display.getWidth()-(2*PADDING)+2,GRID_HEIGHT)
     gfx.setColor(gfx.kColorWhite)
+    gfx.drawRect(FOLDER_1)
+    gfx.drawRect(PROGRESS_1)
+    gfx.drawRect(FOLDER_2)
+    gfx.drawRect(PROGRESS_2)
+    gfx.drawRect(FOLDER_3)
+    gfx.drawRect(PROGRESS_3)
+    gfx.drawRect(FOLDER_4)
+    gfx.drawRect(PROGRESS_4)
+    gfx.drawRect(FOLDER_5)
+    gfx.drawRect(PROGRESS_5)
+    gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
+    gfx.setFont(GameAssets.NORMAL_FONT)
+    gfx.drawText("O1", FOLDER_1.x + FOLDER_1.width/2.5, FOLDER_1.y+2)
+    gfx.drawText("O2", FOLDER_2.x + FOLDER_2.width/2.5, FOLDER_2.y+2)
+    gfx.drawText("O3", FOLDER_3.x + FOLDER_3.width/2.5, FOLDER_3.y+2)
+    gfx.drawText("O4", FOLDER_4.x + FOLDER_4.width/2.5, FOLDER_4.y+2)
+    gfx.drawText("O5", FOLDER_5.x + FOLDER_5.width/2.5, FOLDER_5.y+2)
+    gfx.drawText(frolder1Progress .. "%", PROGRESS_1.x + PROGRESS_1.width/2.5, PROGRESS_1.y+2)
+    gfx.drawText(frolder2Progress .. "%", PROGRESS_2.x + PROGRESS_2.width/2.5, PROGRESS_2.y+2)
+    gfx.drawText(frolder3Progress .. "%", PROGRESS_3.x + PROGRESS_3.width/2.5, PROGRESS_3.y+2)
+    gfx.drawText(frolder4Progress .. "%", PROGRESS_4.x + PROGRESS_4.width/2.5, PROGRESS_4.y+2)
+    gfx.drawText(frolder5Progress .. "%", PROGRESS_5.x + PROGRESS_5.width/2.5, PROGRESS_5.y+2)
 
     --start coord
     --remove top round
     gfx.fillRect(PADDING,FOLDER_HEIGHT,playdate.display.getWidth()-8, playdate.display.getHeight()-FOLDER_HEIGHT-(3*PADDING))
     gfx.fillRoundRect(PADDING,FOLDER_HEIGHT,playdate.display.getWidth()-8, playdate.display.getHeight()-FOLDER_HEIGHT-PADDING, 5)
     gfx.setImageDrawMode(gfx.kDrawModeFillBlack)
-    gfx.drawText("0x" .. string.format("%x", offsetX) .. " : " .. "0x" .. string.format("%x", offsetY), 150, 220)
+    gfx.drawText("0x" .. string.format("%x", offsetX) .. " : " .. "0x" .. string.format("%x", offsetY), 150, 222)
 
     --screen corner
     gfx.drawRoundRect(PADDING,PADDING,playdate.display.getWidth()-8,playdate.display.getHeight()-(2*PADDING),5)
@@ -137,6 +180,7 @@ function startup()
     --gfx.drawText("hello", 0, 0)
 
     timer = tmr.keyRepeatTimerWithDelay(0,600,render)
+    timer = tmr.keyRepeatTimerWithDelay(0,200,handleInput)
 end
 
 --startup call
@@ -145,8 +189,6 @@ startup();
     Called every frame, add here your game logic
 ]]--
 function playdate.update()
-    --check input
-    handleInput()
     --update all sprites
     gfx.sprite.update()
     --update all timers
