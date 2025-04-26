7d2bf9d94c87e4b184264611cd8e04b10ef8cf86
Marc-Alx <marc-alx@outlook.com>
2025-04-27 00:16:58 +0200

    [improve] reduce screen update

--- source/main.lua
+++ source/main.lua
@@ -31,7 +31,7 @@ FOLDER_HEIGHT = 219
 OFFSET_STEP = 2
 
 GRID_AREA = playdate.geometry.rect.new(15, HEADER_HEIGHT+2, 372, 150)
-PROGRESS_BAR = playdate.geometry.rect.new(12, 9, 300, 14)
+PROGRESS_BAR = playdate.geometry.rect.new(10, 8, 300, 17)
 
 FOLDER_OFFSET = 30
 FOLDER_SPACING = 15
@@ -118,6 +118,8 @@ end
 
 --draw number grid, considering input offset
 function drawGrid(oX, oY)
+    gfx.setColor(gfx.kColorBlack)
+    gfx.fillRect(GRID_AREA)
     playdate.graphics.setDrawOffset(offsetX, offsetY)
     gfx.setScreenClipRect(GRID_AREA)
     for i = 1, WIDTH do
@@ -135,23 +137,15 @@ function drawGrid(oX, oY)
     playdate.graphics.setDrawOffset(0,0)
 end
 
--- render UI
-function render()
-    --update numbers
-    for i = 1, WIDTH do
-        for j = 1, HEIGHT do
-            numbers[i][j]:update()
-        end
-    end   
-    
+-- draw static ui shell
+function drawShell() 
     --playdate.drawFPS(190,0)
     gfx.setColor(gfx.kColorBlack)
     gfx.fillRect(GRID_AREA)
-    
+
     --progress
     gfx.setColor(gfx.kColorWhite)
     gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
-    gfx.drawText(progress .. "% Complete", 30, 11)
     gfx.drawRect(PROGRESS_BAR)
 
     gfx.setColor(gfx.kColorWhite)
@@ -161,9 +155,6 @@ function render()
     GameAssets.LOGO_SMALL:draw(PROGRESS_BAR.width+PROGRESS_BAR.x+45, 6)
     gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
 
-    --grid
-    drawGrid(offsetX, offsetY)
-    
     --start folder
     gfx.drawLine(PADDING,GRID_HEIGHT,playdate.display.getWidth()-(2*PADDING)+2,GRID_HEIGHT)
     gfx.setColor(gfx.kColorWhite)
@@ -177,13 +168,41 @@ function render()
     gfx.drawRect(PROGRESS_4)
     gfx.drawRect(FOLDER_5)
     gfx.drawRect(PROGRESS_5)
-    gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
-    gfx.setFont(GameAssets.NORMAL_FONT)
+
     gfx.drawText("O1", FOLDER_1.x + FOLDER_1.width/2.5, FOLDER_1.y+2)
     gfx.drawText("O2", FOLDER_2.x + FOLDER_2.width/2.5, FOLDER_2.y+2)
     gfx.drawText("O3", FOLDER_3.x + FOLDER_3.width/2.5, FOLDER_3.y+2)
     gfx.drawText("O4", FOLDER_4.x + FOLDER_4.width/2.5, FOLDER_4.y+2)
     gfx.drawText("O5", FOLDER_5.x + FOLDER_5.width/2.5, FOLDER_5.y+2)
+
+    --start coord
+    --remove top round
+    gfx.fillRect(PADDING,FOLDER_HEIGHT,playdate.display.getWidth()-8, playdate.display.getHeight()-FOLDER_HEIGHT-(3*PADDING))
+    gfx.fillRoundRect(PADDING,FOLDER_HEIGHT,playdate.display.getWidth()-8, playdate.display.getHeight()-FOLDER_HEIGHT-PADDING, 5)
+    
+    --screen corner
+    gfx.drawRoundRect(PADDING,PADDING,playdate.display.getWidth()-8,playdate.display.getHeight()-(2*PADDING),5)
+
+end
+
+-- render UI
+function render()
+    --update numbers
+    for i = 1, WIDTH do
+        for j = 1, HEIGHT do
+            numbers[i][j]:update()
+        end
+    end   
+    
+    --progress
+    gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
+    gfx.drawText(progress .. "% Complete", 30, 10)
+    
+    --grid
+    drawGrid(offsetX, offsetY)
+    
+    --start folder
+    gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
     gfx.drawText(frolder1Progress .. "%", PROGRESS_1.x + PROGRESS_1.width/2.5, PROGRESS_1.y+2)
     gfx.drawText(frolder2Progress .. "%", PROGRESS_2.x + PROGRESS_2.width/2.5, PROGRESS_2.y+2)
     gfx.drawText(frolder3Progress .. "%", PROGRESS_3.x + PROGRESS_3.width/2.5, PROGRESS_3.y+2)
@@ -191,15 +210,11 @@ function render()
     gfx.drawText(frolder5Progress .. "%", PROGRESS_5.x + PROGRESS_5.width/2.5, PROGRESS_5.y+2)
 
     --start coord
-    --remove top round
-    gfx.fillRect(PADDING,FOLDER_HEIGHT,playdate.display.getWidth()-8, playdate.display.getHeight()-FOLDER_HEIGHT-(3*PADDING))
-    gfx.fillRoundRect(PADDING,FOLDER_HEIGHT,playdate.display.getWidth()-8, playdate.display.getHeight()-FOLDER_HEIGHT-PADDING, 5)
-    gfx.setImageDrawMode(gfx.kDrawModeFillBlack)
+    gfx.setColor(gfx.kColorWhite)
+    gfx.fillRect(150, 222, 120, 10)
+    gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
     gfx.drawText("0x" .. string.format("%x", offsetX) .. " : " .. "0x" .. string.format("%x", offsetY), 150, 222)
 
-    --screen corner
-    gfx.drawRoundRect(PADDING,PADDING,playdate.display.getWidth()-8,playdate.display.getHeight()-(2*PADDING),5)
-    
 end
 
 --look for input in order to adjust offset
@@ -233,6 +248,7 @@ function startup()
     --delay update to let startup logo display
     tmr.new(2000, function() 
         gfx.clear(gfx.kColorBlack)
+        drawShell()
         tmr.keyRepeatTimerWithDelay(1000,500,render)
         tmr.keyRepeatTimerWithDelay(0,150,handleInput)
     end)
